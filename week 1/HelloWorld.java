a91f68678beebe8d5783bc3830b766d7a6ea74aa
Ismail Mohamed <mismail1568@stu.riarauniversity.ac.ke>
2026-09-23 11:23:48 +0300

    first commit

---
+++ week 1/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+* My first Java program
+* @author Your Name
+* @date Today's Date
+*/
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("Hello, World!");
+System.out.println("My name is: [ismai]");
+System.out.println("I am learning Java at [Riara University]");
+System.out.println("Today's date is: [23rd september 2026]");
+}
+}
